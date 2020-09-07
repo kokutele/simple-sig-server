9a7ae5a69fe5632f3721ca07797a5a1e6faa8086
kensaku komatsu <kensaku.komatsu@gmail.com>
2020-09-07 22:54:00 +0900

    implemented signaling feeature

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const protooServer = require('protoo-server')
 const express = require('express')
+const cors = require('cors')
 const http = require('http')
 const { generateLiveId, generatePeerId } = require('./libs/util')
 
 const db = new Map()
 const app = express()
+
+const port = process.env.PORT || 5000
+
+app.use( cors() )
 
 app.get('/liveness', (req, res) => {
   res.send('woking')
@@ -12,16 +17,11 @@ app.get('/liveness', (req, res) => {
 
 app.get('/live/:liveId', (req, res) => {
   const liveId = req.params.liveId
-  const { sender } = req.query
 
   const obj = db.get(liveId)
-  if( obj && obj.sender === sender ) {
-    const {sender, receivers} = obj
-    res.json({sender, numReceiver: receivers.length})
-  } else if( obj && obj.sender !== sender ) {
-    const mesg = `sender is wrong: ${sender}`
-    console.warn(mesg)
-    res.status(500).send(mesg)
+  if( obj ) {
+    const {sender, receiverPeers} = obj
+    res.json({sender, numReceiver: receiverPeers.size})
   } else {
     const mesg = `cannot find liveId: ${liveId}`
     console.warn(mesg)
@@ -36,7 +36,8 @@ app.post('/live', (req, res) => {
   db.set( liveId, {
     room,
     sender: peerId,
-    receivers: []
+    senderPeer: null,
+    receiverPeers: new Map() // Map<peerId, Peer>
   })
   res.json({liveId, peerId})
 })
@@ -49,7 +50,9 @@ app.get('/liveIds', (req, res) => {
   res.json( ret )
 })
 
-const httpServer = http.createServer(app).listen(3000)
+const httpServer = http.createServer(app).listen(port, () => {
+  console.log(`simple signaling server started on port ${port}`)
+})
 
 const options =
 {
@@ -64,30 +67,67 @@ const server = new protooServer.WebSocketServer(httpServer, options)
 server.on('connectionrequest', async(info, accept, reject) => {
   const url = info.request.url
   const [liveId, _params] = url.slice(1).split("?")
-  let { peerId } = _params.split("&")
+  let { peerId } = _params  ? _params.split("&")
     .reduce( (accum, curr) => {
       const [key, val] = curr.split("=")
       return Object.assign({}, accum, {[key]: val})
-    }, {})
+    }, {}) : { peerId: undefined }
   console.log(`liveId -> ${liveId}, peerId=${peerId}`)
 
-  const { room, sender, receivers } = db.get(liveId)
-
   try {
-    if( !!room ) {
-      if( peerId && sender !== peerId ) {
+    const liveObj = db.get(liveId)
+
+    if( !!liveObj.room ) {
+      let isSender = false
+
+      if( peerId && liveObj.sender !== peerId ) {
         throw new TypeError('You are not registered as sender')
-      } else if( peerId && sender === peerId ) {
+      } else if( peerId && liveObj.sender === peerId ) {
         console.log('connected as the sender', peerId)
+        isSender = true
       } else {
         peerId = generatePeerId()
-        receivers.push(peerId)
         console.log('connected as a receiver', peerId)
       }
       console.log('accept')
       const transport = accept()
-      const peer = await room.createPeer(peerId, transport)
-      _setHandler(peer)
+      const peer = await liveObj.room.createPeer(peerId, transport)
+
+      if( isSender ) {
+        liveObj.senderPeer = peer
+      } else {
+        liveObj.receiverPeers.set( peerId, peer )
+      }
+
+      peer.on('request', async ( req, accept, reject ) => {
+        switch( req.method ) {
+        case 'peerId':
+          accept( peerId )
+          break
+        case 'join':
+        case 'leave':
+        case 'offer':
+          console.log( `request - ${req.method}: dst=${req.data.dst}, src=${req.data.src}, isSender=${isSender}` )
+          const dstPeer = !isSender ?
+            liveObj.senderPeer :
+            liveObj.receiverPeers.get( req.data.dst )
+          const res = await dstPeer.request( req.method, req.data )
+          console.log(`accept for ${req.method}`)
+          accept( res )
+          break
+        default:
+          reject(400, `unknown method '${req.method}'`)
+        }
+      })
+
+      peer.on('notification', async ( req ) => {
+        console.log(`notification - ${req.method}: src=${req.data.src}, dst=${req.data.dst}`)
+        const dstPeer = !isSender ?
+          liveObj.senderPeer :
+          liveObj.receiverPeers.get( req.data.dst )
+        await dstPeer.notify( req.method, req.data )
+        console.log(`notification ${req.method} sent`)
+      })
     } else {
       throw new TypeError(`liveId '${liveId}' does not exists`)
     }
@@ -97,14 +137,3 @@ server.on('connectionrequest', async(info, accept, reject) => {
   }
 })
 
-const _setHandler = (peer) => {
-  peer.on('request', (req, accept, reject) => {
-    switch( req.method ) {
-      case 'peerId':
-        accept( peer.id)
-        break
-      default:
-        reject('unknown method')
-    }
-  })
-}
